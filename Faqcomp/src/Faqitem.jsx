9588f8bb367f84adf9758520a6cc740fc36eb3c4
Renjith <renjithkrishnan316@gmail.com>
2025-01-17 21:43:48 +0530

    faq

---
+++ Faqcomp/src/Faqitem.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react'
+import './Faq.css'
+
+function Faqitem({faq,index}) {
+    const [show,setShow]=useState(false)
+    useEffect(()=>{
+        if(index===0){
+            setShow(true)
+        }
+    },[])
+
+    function handleClick(){
+        setShow((prev)=>!prev)
+    }
+  return (
+    <div className='container-class' onClick={handleClick}>
+        <button className= {show ?'arrow':""}>></button>
+    <div>{faq.question} </div>
+  {show && <div >{faq.answer}</div>}
+    </div>
+  )
+}
+
+export default Faqitem
